956ffe1c69d5c728f807b35d4ad6f7ef05c2ba64
Soo Hyun Mun <soohyunmun@MacBookPro.lan>
2026-04-26 10:54:24 -0700

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: isGithubActions ? '/mywebsite/' : '/'
 });
